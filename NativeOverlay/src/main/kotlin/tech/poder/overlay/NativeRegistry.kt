e82d2f7e8be9e26c98afc8570e081b7868a72f0b
Moocow9m <devmoocow9m@gmail.com>
2021-11-07 17:05:29 -0800

    Stub

---
+++ NativeOverlay/src/main/kotlin/tech/poder/overlay/NativeRegistry.kt
@@ -0,0 +1,10 @@
+package tech.poder.overlay
+
+import java.lang.invoke.MethodHandle
+
+object NativeRegistry {
+    val registery = mutableListOf<MethodHandle>()
+    fun register(discription: FunctionDescription): Int {
+        TODO()
+    }
+}
