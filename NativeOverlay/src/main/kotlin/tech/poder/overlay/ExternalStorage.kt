f1ff5d621dd40afc1ab424728c77bcb27310ef86
camden <me@12oclock.dev>
2021-11-12 01:18:16 -0600

    Testing thingy

--- NativeOverlay/src/main/kotlin/tech/poder/overlay/ExternalStorage.kt
+++ NativeOverlay/src/main/kotlin/tech/poder/overlay/ExternalStorage.kt
@@ -7,14 +7,20 @@ import jdk.incubator.foreign.ResourceScope
 @JvmInline
 value class ExternalStorage(val segment: MemorySegment): AutoCloseable {
     companion object {
+
         val nullChar = "\u0000".toByteArray(Charsets.UTF_16LE)
+
         fun fromString(str: String): ExternalStorage {
+
             val newScope = ResourceScope.newConfinedScope()
+
             str.toByteArray(Charsets.UTF_16LE).let { bytes ->
+
                 val segment = MemorySegment.allocateNative((bytes.size + nullChar.size).toLong(), newScope)
                 bytes.forEachIndexed { index, byte ->
                     MemoryAccess.setByteAtOffset(segment, index.toLong(), byte)
                 }
+
                 val startIndex = bytes.size
                 nullChar.forEachIndexed { index, byte ->
                     MemoryAccess.setByteAtOffset(segment, (startIndex + index).toLong(), byte)
--- NativeOverlay/src/test/kotlin/tech/poder/test/overlay/TestHook.kt
+++ NativeOverlay/src/test/kotlin/tech/poder/test/overlay/TestHook.kt
@@ -12,6 +12,7 @@ import kotlin.test.Test
 
 internal class TestHook {
 
+    /*
     @Test
     fun createWindow() {
         val processes = Callback.getProcesses()
@@ -101,6 +102,53 @@ internal class TestHook {
         checker.interrupt()
         println("Window End")
         Thread.sleep(10000)
+    }*/
+
+    @Test
+    fun drawCat() {
+
+        val processes = Callback.getProcesses()
+        var i = 0
+        processes.forEachIndexed { index, process ->
+            if (process.exeLocation.contains("Overwatch.exe")) {
+                i = index
+            }
+            println("$index: ${process.title}(${process.exeLocation})")
+        }
+        println("Choose: $i")
+        val selected = processes[i]
+        //Thread.sleep(10000)
+        val clazz = WindowClass.define("Kats")
+        println(selected.rect)
+        val window = WindowManager.createWindow(
+            WindowManager.WS_EX_TOPMOST or WindowManager.WS_EX_TRANSPARENT or WindowManager.WS_EX_LAYERED,
+            clazz = clazz,
+            windowName = "LiquidOverlay",
+            style = WindowManager.WS_POPUP.toInt(),
+            x = selected.rect.left.toInt(),
+            y = selected.rect.top.toInt(),
+            width = selected.rect.width.toInt(),
+            height = selected.rect.height.toInt()
+        )
+        val rectScope = RectReader.createSegment()
+        val selectedWindow = selected.asWindow()
+
+
+        val overlay = OverlayImpl(window, selectedWindow) {
+
+            val bufferedImage = BufferedImage(it.canvasWidth, it.canvasHeight, BufferedImage.TYPE_INT_RGB)
+            val graphics = bufferedImage.createGraphics()
+
+            graphics.color = Color(0, 100, 0)
+            graphics.fillRect(0, 0, bufferedImage.width, bufferedImage.height)
+
+            it.image(bufferedImage, Overlay.Position(0, 0), it.canvasWidth, it.canvasHeight)
+
+            graphics.dispose()
+
+        }
+
+        window.doLoop()
     }
 
 
