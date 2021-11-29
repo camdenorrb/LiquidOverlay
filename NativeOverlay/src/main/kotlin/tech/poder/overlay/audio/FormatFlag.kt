19258c42a2a2cf31124c9c9f76b17a38222008d7
Moocow9m <devmoocow9m@gmail.com>
2021-11-29 12:48:24 -0800

    Add placeholder GUID

--- NativeOverlay/src/main/kotlin/tech/poder/overlay/audio/FormatFlag.kt
+++ NativeOverlay/src/main/kotlin/tech/poder/overlay/audio/FormatFlag.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 
 enum class FormatFlag(val baseFlag: Short, val subformatGUID: UUID) {
-    PCM(1, TODO()), //TODO, get GUID of KSDATAFORMAT_SUBTYPE_PCM
+    PCM(1, UUID.randomUUID()), //TODO, get GUID of KSDATAFORMAT_SUBTYPE_PCM
     ;
     companion object {
         const val extendedBaseFlag: Short = -2
