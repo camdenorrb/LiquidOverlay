1b75d5a01bf374d7d63ffd574f57ea9487752ca1
Camden <me@12oclock.dev>
2021-12-01 05:13:48 -0600

    Add audio test back

--- NativeOverlay/src/test/kotlin/tech/poder/test/overlay/TestHook.kt
+++ NativeOverlay/src/test/kotlin/tech/poder/test/overlay/TestHook.kt
@@ -1,13 +1,17 @@
 package tech.poder.test.overlay
 
+import jdk.incubator.foreign.MemoryAccess
 import tech.poder.overlay.api.WinAPI
+import tech.poder.overlay.audio.AudioFormat
+import tech.poder.overlay.audio.FormatData
 import tech.poder.overlay.overlay.BasicOverlay
 import tech.poder.overlay.overlay.base.Overlay
 import tech.poder.overlay.window.WindowClass
 import tech.poder.overlay.window.WindowManager
 import java.awt.Color
 import java.awt.image.BufferedImage
-import kotlin.math.ceil
+import kotlin.experimental.and
+import kotlin.math.min
 import kotlin.test.Test
 
 internal class TestHook {
@@ -103,20 +107,76 @@ internal class TestHook {
         overlay.close()
     }
 
-
     @Test
     fun basicAudio() {
-        val pDevice = WinAPI.getDefaultAudioDevice()
-        val pAudioClient = WinAPI.activateAudioClient(pDevice)
-        val mixInfo = WinAPI.getMixFormat(pAudioClient)
-        val bufferFrameCount = WinAPI.getBufferSize(pAudioClient)
-        val pRenderClient = WinAPI.getService(pAudioClient)
-        WinAPI.start(pRenderClient)
-        val hnsPeriod = WinAPI.getActualDuration(mixInfo, bufferFrameCount)
-        while (true) {
-            Thread.sleep(ceil(hnsPeriod).toLong())
-
+        val state = WinAPI.newState()
+        WinAPI.startRecording(state)
+        val hnsPeriod = MemoryAccess.getDoubleAtOffset(state.segment, state[3])
+        val sleepTime = ((hnsPeriod / 10_000.0) / 2.0).toLong()
+        var counter = 0
+        val format = AudioFormat.getFormatData(WinAPI.getFormat(state))
+        println(AudioFormat.getFormat(format))
+        val bytesPerFrame = format.blockAlignment//((getBitsPerSample(format) * getNumberOfChannels(format).toLong()) / 8L).toInt()
+        val framesPerSecond = format.sampleRate
+        val bytesPerSecond = bytesPerFrame * framesPerSecond.toLong()
+        if (bytesPerSecond.toInt().toLong() != bytesPerSecond) {
+            error("Bytes per second is too big")
         }
+
+        while (counter < 30) {
+            println("at top of loop")
+            // Sleep for half the buffer duration.
+            Thread.sleep(sleepTime)
+            counter++
+            WinAPI.getNextPacketSize(state)
+            var packetLength = WinAPI.getPNumFramesInPacket(state)
+            while (packetLength != 0u) {
+                WinAPI.getBuffer(state)
+
+                val flags = WinAPI.getPFlags(state)
+                if (flags and WinAPI.AUDCLNT_BUFFERFLAGS_SILENT != 0.toByte()) {
+                    println("SILENT")
+                }
+                val amountOfFramesInBuffer = WinAPI.getPNumFramesInPacket(state)
+                val pDataLocation = WinAPI.getPData(state, amountOfFramesInBuffer.toLong() * bytesPerFrame.toLong())
+                var currentPos = 0L
+                while (currentPos < pDataLocation.byteSize()) {
+                    val buffer =
+                        pDataLocation.asSlice(currentPos, min(bytesPerSecond, pDataLocation.byteSize() - currentPos))
+                    processFrame(buffer.toByteArray(), format)
+                    currentPos += buffer.byteSize()
+                }
+                WinAPI.releaseBuffer(state)
+                WinAPI.getNextPacketSize(state)
+                packetLength = WinAPI.getPNumFramesInPacket(state)
+            }
+        }
+    }
+
+
+    fun processFrame(buffer: ByteArray, format: FormatData) {
+        /*
+        val data: AudioChannels = when(format.bitsPerChannel.toInt()) {
+            8 -> {
+                PCMByteAudioChannels.process(buffer, format)
+            }
+            16 -> {
+                PCMShortAudioChannels.process(buffer, format)
+            }
+            32 -> {
+                if (format.tag == FormatFlag.IEEE_FLOAT) {
+                    FloatAudioChannels.process(buffer, format)
+                } else {
+                    PCMIntAudioChannels.process(buffer, format)
+                }
+            }
+            else -> {
+                error("Unknown format $format")
+            }
+        }*/
+        //process speech
+
+        //end process speech
     }
 
 
